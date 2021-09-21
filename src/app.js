c0ef13001045596a30b28ea53fc572f0731d28b0
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2021-09-21 17:01:33 +0400

    fixed standard error handling

--- src/app.js
+++ src/app.js
@@ -11,4 +11,11 @@ app.set('models', sequelize.models);
 
 app.use(router);
 
+app.use((error, req, res, next) => {
+  console.error(error); // not okay inn production, but this is test task
+  res.status(500).json({
+    error: error.message,
+  });
+});
+
 module.exports = app;
